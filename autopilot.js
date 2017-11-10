54cb21d54abc260f45e99dfe93e05f025eb9cf42
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-11-10 00:22:39 -0500

    translated 6 next functions, tried switch instead of if statement for destination

--- autopilot.js
+++ autopilot.js
@@ -11,3 +11,33 @@ function addCar(cars, newCar) {
   cars.push(newCar);
   console.log("Adding new car to fleet. Fleet size is now " + cars.length + ".");
 }
+
+function pickUpPassenger(car) {
+  car.passenger += 1;
+  car.gas -= 10;
+}
+
+// tried with switch
+function getDestination(car) {
+  switch (car.city) {
+    case 'Toronto':
+      return 'Mississauga'
+      break;
+    case 'Mississauga':
+      return 'London'
+      break;
+    case 'London':
+      return 'Toronto'
+      break;
+  }
+}
+
+function getGasDisplay() {
+
+}
+
+function fillUpGas(car) {
+  var oldGas = car.gas
+  car.gas = 100
+  console.log("Filled up to " + getGasDisplay(car.gas) + " on gas from " + getGasDisplay(oldGas) + ".");
+}
