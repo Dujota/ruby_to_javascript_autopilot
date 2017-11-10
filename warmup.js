e97b49eadb6142e72597e7da1501da2daeda3eed
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-11-09 22:28:10 -0500

    started working through the warmup syntax

---
+++ warmup.js
@@ -0,0 +1,31 @@
+// 1
+
+var apples = 14;
+
+console.log(apples);
+
+console.log("I have " + apples + " apples");
+
+
+// 2
+
+var materials = ['wood', 'metal', 'stone'];
+
+var words = {
+  'elephant': 'the worlds largest land animal.',
+  'school': 'A place of learning.',
+  'ice cream': 'A delicious milk-based dessert.'
+};
+
+console.log(words);
+console.log(materials);
+
+// 3
+var num = 16;
+if (num > 10) {
+  console.log(num + " is greater than 10.");
+} else if (num === 10) {
+  console.log(num + " is exactly 10.");
+} else {
+  console.log(num +" must be less than 10.");
+}
